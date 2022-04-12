8bbcd3737ed42d27e5c3f06e6c1e2dde5eae53e8
YusukeShimizuEng <ajmdwtpgajdm@gmail.com>
2022-04-12 22:35:09 +0900

    java_github_コミットテスト

---
+++ src/github_java/Practice.java
@@ -0,0 +1,28 @@
+package github_java;
+
+public class Practice {
+
+/*
+ * alt + shift + x  →  j
+ * または
+ * ctrl + F11
+ * でプログラム実行
+ * */
+
+	public static void main(String[] args) {
+		try {
+			int a = 8;
+			if(a == 10) {
+				throw new Exception();
+			}
+			System.out.println("例外じゃないよ！");
+		}
+		catch(Exception e){
+			System.out.println("例外発生！");
+		}
+
+
+	}
+
+}
+
